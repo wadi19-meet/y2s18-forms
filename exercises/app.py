96969554c957b814b5442614ff3544d74a3b4a85
aboud20-meet <aboud20@meet.mit.edu>
2018-08-09 12:00:06 +0300

    finish labs

--- exercises/app.py
+++ exercises/app.py
@@ -9,17 +9,26 @@ def home():
 @app.route('/student/<int:student_id>')
 def display_student(student_id):
     return render_template('student.html', student=query_by_id(student_id))
+    
 @app.route('/add', methods=['GET' , 'POST'])
 def add_student_route():
     if request.method == 'GET':
         return render_template('add.html')
     else:
-        print('recieved post request')                 
+        print('recieved post request')
+        print(request.form['student_name'])
+        print(request.form['student_name'])
+
         add_student(
-            name = request.form['student_name'],
-            name = request.form['studeent.year'])
+            request.form['student_name'],
+            request.form['student_year'],False)
             
         
         return  render_template('add.html')
+@app.route('/delete/<int:student_id>', methods=['POST'])
+def delete_student_id(student_id):
+    session.query(Student).filter_by(student_id=student_id).delete()
+    return redirect(url_for('home'))
+    
 
 app.run(debug=True)
